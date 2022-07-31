f3fe65a9f0d6828649abfb894a598c0fd66df6c1
Siddhant <santoshshrivastava39@gmail.com>
2022-08-01 01:00:15 +0530

    Selection practice problems with if & Else

--- PracticeProblem.js
+++ PracticeProblem.js
@@ -1,47 +1,115 @@
-//1-Use random function to get single digit
-let singledigitnumber = Math.floor(Math.random() * 10);  
-console.log("Single digit number is "+singledigitnumber);
+//1- Generate 5 digit random numbers then output the min and max value
+let num1 = Math.floor(Math.random()*900)+100;
+let num2 = Math.floor(Math.random()*900)+100;
+let num3 = Math.floor(Math.random()*900)+100;
+let num4 = Math.floor(Math.random()*900)+100;
+let num5 = Math.floor(Math.random()*900)+100;
+console.log("Three digit random numbers are "+ num1,num2,num3,num4,num5);
 
-
-//2-Use Random to get Dice Number between 1 to 6
-let dicenumber = Math.floor(Math.random() * 6)+1;  
-console.log("Random Dice Number is "+dicenumber);
-
-//3- Add two Random Dice Number and Print the Result
-let dicenumber1 = Math.floor(Math.random() * 6)+1;  
-let dicenumber2 = Math.floor(Math.random() * 6)+1;  
-console.log("Random number on Dice Number1 is "+dicenumber1);
-console.log("Random number on Dice Number2 is "+dicenumber2);
-console.log("Addition of 2 random dice numbers is "+(dicenumber1+dicenumber2));
-
-
-//4-Generate 2 digit random number then find their sum and the average
-let number = 0;
-let sum = 0;
-let countOfRandommNumbers = 5;
-
-for(let i = 0; i < countOfRandommNumbers; i++){
-    number = Math.floor(Math.random()*100);
-    sum = sum + number;
+//For finding Max Number
+if(num1>num2 && num1>num3 && num1>num4 && num1>num5)
+{
+    console.log(num1 + " is maximum number");
 }
-let average = sum / countOfRandommNumbers;
-console.log("The sum of 5 random two digit number is :" + sum);
-console.log("The average of 5 random two digit number is :" + average);
+if(num2>num1 && num2>num3 && num2>num4 && num2>num5)
+{
+    console.log(num2 + " is maximum number");
+}
+if(num3>num1 && num3>num2 && num3>num4 && num3>num5)
+{
+    console.log(num3 + " is maximum number");
+}
+if(num4>num1 && num4>num2 && num4>num3 && num4>num5)
+{
+    console.log(num4 + " is maximum number");
+}
+if(num5>num2 && num5>num3 && num5>num4 && num5>num1)
+{
+    console.log(num5 + " is maximum number");
+}
+//For finding Min Number
+if(num1<num2 && num1<num3 && num1<num4 && num1<num5)
+{
+    console.log(num1 + " is minimum number");
+}
+if(num2<num1 && num2<num3 && num2<num4 && num2<num5)
+{
+    console.log(num2 + " is minimum number");
+}
+if(num3<num1 && num3<num2 && num3<num4 && num3<num5)
+{
+    console.log(num3 + " is minimum number");
+}
+if(num4<num1 && num4<num2 && num4<num3 && num4<num5)
+{
+    console.log(num4 + " is minimum number");
+}
+if(num5<num2 && num5<num3 && num5<num4 && num5<num1)
+{
+    console.log(num5 + " is minimum number");
+}
 
 
-//5- Unit Conversion
-  //a- Inch to Ft
-  let a=42;
-  let result1=a/12;
-  console.log("42 in = "+result1+" ft");
-  
+//2-Check whether the generated Day & Month falls between  given Day & Month or not
+console.log("Check day of month is between March 20 and June 20");
 
-  //b- Rectangular Plot of 60 feet x 40 feet in meters
-  let AreaInFeet=60*40;
-  let result2=AreaInFeet/10.764;
-  console.log("Plot area in meter : "+result2);
-   
-  //c- Calculate area of 25 such plots in acres
-  let result3=AreaInFeet*25/43560;
-   console.log("25 plots area in acres is : "+result3);
+let day=(Math.floor(Math.random() * 10)%31)+1;
+let month=(Math.floor(Math.random() * 10)%6)+1;
 
+console.log("Day : "+day+" & Month : "+month);
+if ( day> 20 && day< 31 && month == 3 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 30 && month == 4 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 31 && month == 5 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 20 && month == 6 )
+{
+    console.log("Input date is within range.");
+}
+else
+{
+    console.log("False");
+}
+
+//3-Check Whether the input year is a leap year or not..
+var readlineSync = require('readline-sync');
+
+var year = readlineSync.question("Enter the year :")
+
+if (year % 400 == 0)
+{
+    console.log("It is a leap year");
+
+}
+else if (year % 100 == 0)
+{
+    console.log(" It is not a leap year");
+}
+else if (year % 4 == 0)
+{
+    console.log("It is a leap year");
+}
+else
+{
+    console.log("It is not a leap year");
+}
+
+
+//4- Program to simulate a coin flip and print out Head or Tail
+
+let tail = Math.floor(Math.random()*10) % 2;
+if(tail == 0)
+{
+    console.log("Tail");
+}
+else
+{
+    console.log("Head");
+}
