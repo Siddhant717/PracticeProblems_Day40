4bd8866d8796dfa1331fca8be43c857c17a7cdcc
Siddhant <santoshshrivastava39@gmail.com>
2022-08-01 13:18:21 +0530

    Repetition problems with for loop

--- PracticeProblem.js
+++ PracticeProblem.js
@@ -1,149 +1,65 @@
-//1- Read a single digit number and write the number in word using Case
-let Num = Math.floor(Math.random()*10)%10;
-console.log("Number : "+ Num);
-
-switch(Num){
-    case 0:
-        console.log("Zero");
-        break;
-    case 1:
-        console.log("One");
-        break;
-    case 2:
-        console.log("Two");
-        break;
-    case 3:
-        console.log("Three");
-        break;
-    case 4:
-        console.log("Four");
-        break;
-    case 5:
-        console.log("Five");
-        break;
-    case 6:
-        console.log("Six");
-        break;
-    case 7:
-        console.log("Seven");
-        break;
-    case 8:
-        console.log("Eight");
-        break;
-    case 9:
-        console.log("Nine");
-        break;
-    default:
-        console.log("Invalid number");
-        break;
+//1-prints a table of the powers of 2
+var readlineSync = require('readline-sync');
+var range = readlineSync.question("Enter range to find power of two: ");
+var sum = 1
+for (var i = 1; i <= range; i++) {
+    sum = sum * 2;
 }
 
-//2-Read a Number and Display the week day (Sunday, Monday,...)
-let DayNumber = Math.floor(Math.random()*7) + 1;
-console.log("Number : "+DayNumber);
-switch(DayNumber){
-    case 1:
-        console.log("Sunday");
-        break;
-    case 2:
-        console.log("Monday");
-        break;
-    case 3:
-        console.log("Tuesday");
-        break;
-    case 4:
-        console.log("Wednesday");
-        break;
-    case 5:
-        console.log("Thursday");
-        break;
-    case 6:
-        console.log("Friday");
-        break;
-    case 7:
-        console.log("Saturday");
-        break;
-    default:
-        console.log("Invalid number");
-        break;
+console.log("Power of two table upto the range is: ", sum);
+
+//2-Print the Harmonic Number
+var n = parseInt(readlineSync.question("Enter a number :"));
+var sum = 0;
+
+for (var i = 1; i <= n; i++) {
+    sum = sum + (1 / i);
 }
 
-//3- Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
-let power = Math.floor(Math.random()*10)%7;
-let Numb = 10**power;
-console.log("Number : "+Numb);
-switch(Numb){
-    case 1:
-        console.log("One");
-        break;
-    case 10:
-        console.log("Ten");
-        break;
-    case 100:
-        console.log("Hundred");
-        break;
-    case 1000:
-        console.log("Thousand");
-        break;
-    case 10000:
-        console.log("Ten thousand");
-        break;
-    case 100000:
-        console.log("One Lakh");
-        break;
-    case 1000000:
-        console.log("Ten Lakh");
-        break;
-    default:
-        console.log("Not in Range");
-        break;
+console.log("harmonic mean upto " + n + " = " + sum);
+
+//3- Determines if the number is a prime or not
+var number = parseInt(readlineSync.question("Enter a number :"));
+let count = 0;
+for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+        count++;
+    }
+}
+if (count == 2) {
+    console.log("It is a Prime Number");
+}
+else {
+    console.log("It is not a Prime Number");
 }
 
-//4 UnitConversion
-console.log(" enter 1 for Feet to Inch");
-console.log(" enter 2 for Feet to Meter");
-console.log(" enter 3 for Inch to Feet");
-console.log(" enter 4 for Meter to Feet");
+//4-Take a range from user and output the prime numbers in that range
+var lowerrange = parseInt(readlineSync.question("Enter a lower range :"));
+var higherrange = parseInt(readlineSync.question("Enter a higher range :"));
 
+console.log("Prime Numbers between " + lowerrange + "to" + higherrange + "are");
 
-var readlineSync = require('readline-sync');
+for (var i = lowerrange; i <= higherrange; i++) {
+    let flag = 0;
 
-var input = parseInt(readlineSync.question("Enter the number :")) 
-
-switch(input)
-{
-case 1:
-  var readlineSync = require('readline-sync');
-//convert ft to inch
-var input1 = parseInt(readlineSync.question("Enter No in Feet :"))
-let result = 12*input1;
-console.log(input1 +"ft =" +result+"inch");
-break;
-
-//convert ft to m
-case 2:
-var readlineSync = require('readline-sync');
-var input2 = parseInt(readlineSync.question("Enter Number in Feet :"));
-let result1 = 0.3048*input2;
-console.log(input2 +"ft =" +result1+"meter");
-break;
-
-//convert inch to ft
-case 3:
-var readlineSync = require('readline-sync');
-var input3 = parseInt(readlineSync.question("Enter Number in inch :"));
-let result2 = 0.0833*input3;
-console.log(input3 +"inch =" +result2+"ft");
-break;
-
-//convert m to ft
-case 4:
-var readlineSync = require('readline-sync');
-var input4 = parseInt(readlineSync.question("Enter Number in meter :"));
-let result3 = 3.2808*input4;
-console.log(input4 +"meter =" +result3+"ft");
-break;
-default :
-console.log("Enter a valid number");
-break;
+    for (var j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
 }
+
+//5- Compute a factorial of a number taken as input
+var number = parseInt(readlineSync.question("Enter a number :"));
+let fact = 1;
+
+for (var i = 1; i <= number; i++) {
+    fact = fact * i;
+}
+console.log("Factorial of " + number + " is :" + fact);
+
+
