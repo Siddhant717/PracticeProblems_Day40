8b05c0b37d13906a7b2ed98667306457044acfc8
Siddhant <santoshshrivastava39@gmail.com>
2022-08-01 15:18:55 +0530

    Repetition problem with while loop

--- PracticeProblem.js
+++ PracticeProblem.js
@@ -1,65 +1,41 @@
-//1-prints a table of the powers of 2
+//3-FlipCoin
+let FlipCoin=1;
+while(FlipCoin<12)
+{
+    let n= Math.floor(Math.random()*10)%2;
+    if(n==1)
+    {
+        console.log("Head");
+    }
+    else
+    {
+        console.log("Tail");
+    }
+    FlipCoin++;
+}
+
+//4-Find Magic Number
 var readlineSync = require('readline-sync');
-var range = readlineSync.question("Enter range to find power of two: ");
-var sum = 1
-for (var i = 1; i <= range; i++) {
-    sum = sum * 2;
-}
+var n = readlineSync.question("Enter number between 1 to 100 ");
 
-console.log("Power of two table upto the range is: ", sum);
-
-//2-Print the Harmonic Number
-var n = parseInt(readlineSync.question("Enter a number :"));
-var sum = 0;
-
-for (var i = 1; i <= n; i++) {
-    sum = sum + (1 / i);
-}
-
-console.log("harmonic mean upto " + n + " = " + sum);
-
-//3- Determines if the number is a prime or not
-var number = parseInt(readlineSync.question("Enter a number :"));
-let count = 0;
-for (var i = 1; i <= number; i++) {
-    if (number % i == 0) {
-        count++;
-    }
-}
-if (count == 2) {
-    console.log("It is a Prime Number");
-}
-else {
-    console.log("It is not a Prime Number");
-}
-
-//4-Take a range from user and output the prime numbers in that range
-var lowerrange = parseInt(readlineSync.question("Enter a lower range :"));
-var higherrange = parseInt(readlineSync.question("Enter a higher range :"));
-
-console.log("Prime Numbers between " + lowerrange + "to" + higherrange + "are");
-
-for (var i = lowerrange; i <= higherrange; i++) {
-    let flag = 0;
-
-    for (var j = 2; j < i; j++) {
-        if (i % j == 0) {
-            flag = 1;
-            break;
+function FindMagicNumber(n){
+  let r,s;
+    while(n>9)
+    {
+        while(n>0){
+            r=n%10;
+            s=s+r;
+            n=n/10;
+           
         }
+        n=s;
+        s=0;
     }
-    if (i > 1 && flag == 0) {
-        console.log(i);
+    if(n==1){
+        console.log("Its a magic number")
+    }
+    else{
+        console.log("Its not a magic number")
     }
 }
-
-//5- Compute a factorial of a number taken as input
-var number = parseInt(readlineSync.question("Enter a number :"));
-let fact = 1;
-
-for (var i = 1; i <= number; i++) {
-    fact = fact * i;
-}
-console.log("Factorial of " + number + " is :" + fact);
-
-
+FindMagicNumber(n);
